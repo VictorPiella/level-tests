97ae66156454b229d4fbe12aaee0320aa2f0832c
Victor Piella <vpiella28@gmail.com>
2021-02-15 08:44:49 +0100

    Add more tests

---
+++ Pass the tests JS/README.md
@@ -0,0 +1,16 @@
+## Javascript questions
+
+There are 4 functions that you will need to complete to modify the super hero data provided in 
+`javascript-questions/super-heroes.json`
+
+### Setup
+
+```
+cd javascript-questions
+yarn install
+yarn test
+```
+
+### Finished result
+
+All of the tests in the `/javascript-questions/test` directory should pass.
---
+++ Pass the tests JS/javascript-questions/index.js
@@ -0,0 +1,84 @@
+const heroes = require('./super-heroes.json');
+
+/**
+ * Will find all of the marvel characters in the data and return them as an array
+ * @returns [
+ *  {
+ *    ...hero,
+ *    publisher: 'Marvel'
+ *  }
+ * ]
+ */
+const getDCHeroes = (heroes) => {
+  return (heroes.filter((x)=>x.publisher === "DC Comics"));
+}
+
+/**
+ * Converts the character property into an array
+ * @returns [
+ *  {
+ *    ...hero,
+ *    character: [
+ *      string,
+ *      string,
+ *      string,
+ *    ]
+ *  }
+ * ]
+ */
+const convertCharactersToArray = (heroes) => {
+  return (heroes.map(hero => {
+    return { ...hero,
+      characters: hero.characters.split(","),
+    }
+  }));
+}
+
+/**
+ * Coverts super hero data so it is grouped the publisher property
+ * @returns {
+ *   "publisher 1": [
+ *     { ...hero }
+ *     ...
+ *   ],
+ *   "publisher 2" :[
+ *     {... hero}
+ *     ...
+ *   ]
+ * }
+ */
+const groupByPublisher = (heroes) => {
+  return heroes.reduce((previousValue, heroe) => {
+    return {
+      ...previousValue,
+      [heroe.publisher]: [
+        ...(previousValue[heroe.publisher] || []),
+        heroe,
+      ],
+    }
+  }, {})
+}
+
+/**
+ * Returns and array of DC Comic heroes that have more than 1 character
+ * @returns [
+ *  {
+ *    ...hero,
+ *    character: [
+ *      string,
+ *      string,
+ *      string,
+ *    ]
+ *  }
+ * ]
+ */
+const getDCHeroesWithMoreThanOneCharacter = (heroes) => {
+  return convertCharactersToArray(heroes).filter((heroe) => heroe.characters.length > 1 );
+}
+
+module.exports = {
+  convertCharactersToArray,
+  getDCHeroes,
+  getDCHeroesWithMoreThanOneCharacter,
+  groupByPublisher,
+}
